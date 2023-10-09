ba4fc6275f1e3ff3bf14953ba592706b278b593c
unknown <lucas.silva@germinare.org.br>
2023-10-08 22:34:00 -0300

    Adicionando rota de envio de Sms

--- src/main/java/com/example/api_busco/Models/ApiResponse.java
+++ src/main/java/com/example/api_busco/Models/ApiResponse.java
@@ -1,47 +1,17 @@
 package com.example.api_busco.Models;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse {
     private boolean responseSucessfull;
     private String description;
     private List<Usuarios> object;
-
-    public boolean isResponseSucessfull() {
-        return responseSucessfull;
-    }
-
-    public void setResponseSucessfull(boolean responseSucessfull) {
-        this.responseSucessfull = responseSucessfull;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Object getObject() {
-        return object;
-    }
-
-    public void setObject(List<Usuarios> object) {
-        this.object = object;
-    }
-
-    @Override
-    public String toString() {
-        return "ApiResponse{" +
-                "responseSucessfull=" + responseSucessfull +
-                ", description='" + description + '\'' +
-                ", object=" + object +
-                '}';
-    }
+    private Object aditionalInformation;
 }
--- src/main/java/com/example/api_busco/Services/UsuarioService.java
+++ src/main/java/com/example/api_busco/Services/UsuarioService.java
@@ -5,10 +5,11 @@ import com.example.api_busco.Models.Usuarios;
 import com.example.api_busco.Repositorys.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UsuarioService {
@@ -22,9 +23,9 @@ public class UsuarioService {
     public ApiResponse listarUsuarios(){
         try {
             List<Usuarios> usuarios = usuarioRepository.findAll();
-            return new ApiResponse(true, "Usuários retornados com sucesso", usuarios);
+            return new ApiResponse(true, "Usuários retornados com sucesso", usuarios, null);
         }catch (Exception exception){
-            return new ApiResponse(false, "Falha ao retornar usuários", null);
+            return new ApiResponse(false, "Falha ao retornar usuários", null, null);
         }
     }
 
@@ -32,9 +33,9 @@ public class UsuarioService {
         try {
             Usuarios usuarioResponse = usuarioRepository.save(usuario);
             List<Usuarios> usuariosList = Collections.singletonList(usuarioResponse);
-            return new ApiResponse(true, "Usuário inserido com sucesso", usuariosList);
+            return new ApiResponse(true, "Usuário inserido com sucesso", usuariosList, null);
         }catch (Exception exception){
-            return new ApiResponse(false, "Usuário já existe no banco", null);
+            return new ApiResponse(false, "Usuário já existe no banco", null, null);
         }
     }
 
@@ -43,12 +44,40 @@ public class UsuarioService {
             Usuarios usuario = usuarioRepository.findByEmail(email);
             List<Usuarios> usuariosList = Collections.singletonList(usuario);
             if (usuario != null && usuario.getEmail().equals(email) && usuario.getSenha().equals(senha)){
-                return new ApiResponse(true, "Usuário logado com sucesso", usuariosList);
+                return new ApiResponse(true, "Usuário logado com sucesso", usuariosList, null);
             }else{
-                return new ApiResponse(false, "Email ou senha incorretos", null);
+                return new ApiResponse(false, "Email ou senha incorretos", null, null);
             }
         }catch (Exception exception){
-            return new ApiResponse(false, "Falha ao realizar login", null);
+            return new ApiResponse(false, "Falha ao realizar login", null, null);
+        }
+    }
+
+    public ApiResponse enviarSms(String numero){
+        try {
+            Random random = new Random();
+            StringBuilder numeroAleatorio = new StringBuilder();
+            for (int i = 0; i < 4; i++) {
+                int digito = random.nextInt(10);
+                numeroAleatorio.append(digito);
+            }
+            String numeroAleatorioString = numeroAleatorio.toString();
+
+            Twilio.init(
+                    "ACba58308af84af72f72e9b8eb099f9b9f",
+                    "053b04ea91e4d5d98f1cf4681bfb51e1");
+
+            Message.creator(
+                            new PhoneNumber("+55" + numero),
+                            new PhoneNumber("+12566459677"),
+                            "Seu código de verificação é " + numeroAleatorioString)
+                    .create();
+
+            Map<Object, Object> jsonMap = new HashMap<>();
+            jsonMap.put("code", numeroAleatorioString);
+            return new ApiResponse(true, "Código de verificação enviado com sucesso", null, jsonMap);
+        }catch (Exception exception){
+            return new ApiResponse(false, "Falha ao enviar código de verificação para " + numero, null, null);
         }
     }
 
@@ -58,12 +87,12 @@ public class UsuarioService {
             if (usuarioFound.isPresent()){
                 usuarioRepository.deleteById(id);
                 List<Usuarios> usuario = Collections.singletonList(usuarioFound.get());
-                return new ApiResponse(true, "Usuário deletado com sucesso", usuario);
+                return new ApiResponse(true, "Usuário deletado com sucesso", usuario, null);
             }else{
-                return new ApiResponse(false, "Usuário não encontrado no banco", null);
+                return new ApiResponse(false, "Usuário não encontrado no banco", null, null);
             }
         }catch (Exception exception){
-            return new ApiResponse(false, "Falha ao deletar usuário", null);
+            return new ApiResponse(false, "Falha ao deletar usuário", null, null);
         }
     }
 }
