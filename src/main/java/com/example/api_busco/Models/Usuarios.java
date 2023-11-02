ccc34164657845c9dc31e73512b16aa69c73e011
Lucas Guilherme da Silva <lucassilva-ieg@instituto.ad>
2023-11-02 20:10:07 -0300

    retornando a data de cadastro do usuário

--- src/main/java/com/example/api_busco/Models/Usuarios.java
+++ src/main/java/com/example/api_busco/Models/Usuarios.java
@@ -4,6 +4,8 @@ package com.example.api_busco.Models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,16 +22,20 @@ public class Usuarios {
     private String cep;
     private String nome;
     private String cpf;
+    private Date data_cadastro;
     private int qnt_doacao;
     private String telefone;
     private String foto;
 
-    public Usuarios(String email, String senha, String cep, String nome, String cpf,  String telefone) {
+    public Usuarios(String email, String senha, String cep, String nome, String cpf, Date data_cadastro, int qnt_doacao, String telefone, String foto) {
         this.email = email;
         this.senha = senha;
         this.cep = cep;
         this.nome = nome;
         this.cpf = cpf;
+        this.data_cadastro = data_cadastro;
+        this.qnt_doacao = qnt_doacao;
         this.telefone = telefone;
+        this.foto = foto;
     }
 }
